62b8d3e26f1c45f62b1c3a5df9ae312afee4f648
hirenballewar <33598588+hirenballewar@users.noreply.github.com>
2020-05-13 18:05:36 +0100

    Company App Template using React

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <a href="#myPage" title="To Top">
                 <span className="glyphicon glyphicon-chevron-up"></span>
                 </a>
-                    <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
+                  
             </footer>
     )
 }
