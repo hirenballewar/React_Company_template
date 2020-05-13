a41bb252e6227a7333daa3d6a3ba811b09386b96
hirenballewar <33598588+hirenballewar@users.noreply.github.com>
2020-05-13 17:14:56 +0100

    This is a Company Template made in React

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Map = () => {
+    return(
+            <img src="https://i.ibb.co/0XfTd4Q/map.jpg" 
+            className="w3-image w3-greyscale-min" 
+            style={{width:"100%"}} />
+
+    );
+};
+
+export default Map;
