a7c84e79b897c2023c01aacc966978f15f10f3d2
hirenballewar <33598588+hirenballewar@users.noreply.github.com>
2020-05-13 17:20:38 +0100

    Updates

---
+++ precache-manifest.99f6b5058612b9152d45bc65afc427b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5288f0328bb961f7a896bf2ebc2a61d8",
+    "url": "/React_Company_template/index.html"
+  },
+  {
+    "revision": "de822b89c501eba9ef68",
+    "url": "/React_Company_template/static/css/main.3f015b8f.chunk.css"
+  },
+  {
+    "revision": "c055961eabf518e2bb2e",
+    "url": "/React_Company_template/static/js/2.4df74f26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Company_template/static/js/2.4df74f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de822b89c501eba9ef68",
+    "url": "/React_Company_template/static/js/main.539790cc.chunk.js"
+  },
+  {
+    "revision": "cb6e5ce7e2c636396682",
+    "url": "/React_Company_template/static/js/runtime-main.1bbcc5fb.js"
+  }
+]);
